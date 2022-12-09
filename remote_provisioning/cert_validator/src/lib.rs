cb6ced2551a749422ddf193d6ec95560008c0800
Andrew Scull <ascull@google.com>
2022-12-09 18:03:20 +0000

    Report ciborium errors when parsing buffers and files
    The helpers that parse CBOR buffers and files using ciborium can
    directly return those errors rather than converting to anyhow errors. We
    add a semantic error when the parsed CBOR item doesn't use the entirety
    of the available data.
    
    Test: atest libcert_request_validator_tests
    Change-Id: Ida6b9cee8a5c31af91dff53a4fbbeaa056471dc3

--- remote_provisioning/cert_validator/src/lib.rs
+++ remote_provisioning/cert_validator/src/lib.rs
@@ -6,24 +6,25 @@ pub mod dice;
 mod display;
 pub mod publickey;
 
-use anyhow::{bail, Context, Result};
 use ciborium::{de::from_reader, value::Value};
 use std::io::Read;
 
+type CiboriumError = ciborium::de::Error<std::io::Error>;
+
 /// Reads the provided binary cbor-encoded file and returns a
 /// ciborium::Value struct wrapped in Result.
-pub fn value_from_file(fname: &str) -> Result<Value> {
+pub fn value_from_file(fname: &str) -> Result<Value, CiboriumError> {
     let bytes = std::fs::read(fname)?;
-    value_from_bytes(&bytes).with_context(|| format!("Decoding {} failed", fname))
+    value_from_bytes(&bytes)
 }
 
 /// Decodes the provided binary CBOR-encoded value and returns a
 /// ciborium::Value struct wrapped in Result.
-pub fn value_from_bytes(mut bytes: &[u8]) -> Result<Value> {
-    let value = from_reader(bytes.by_ref()).context("Decoding CBOR failed")?;
+pub fn value_from_bytes(mut bytes: &[u8]) -> Result<Value, CiboriumError> {
+    let value = from_reader(bytes.by_ref())?;
     // Ciborium tries to read one Value, but doesn't care if there is trailing data. We do.
     if !bytes.is_empty() {
-        bail!("Unexpected trailing data");
+        return Err(CiboriumError::Semantic(Some(0), "unexpected trailing data".to_string()));
     }
     Ok(value)
 }
@@ -31,6 +32,7 @@ pub fn value_from_bytes(mut bytes: &[u8]) -> Result<Value> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use anyhow::Result;
 
     #[test]
     fn value_from_bytes_valid_succeeds() -> Result<()> {
