6590bf190e2f9832eca5c25122c5c74cc4646048
Andrew Scull <ascull@google.com>
2023-05-02 09:46:09 +0900

    Use structured errors for FieldValue
    Replace anyhow with thiserror for FieldValue in an effort towards more
    flexible handling of errors in libhwtrust.
    
    Test: atest --host hwtrust_cli_tests hwtrust_tests libhwtrust_tests
    Bug: 298580435
    (cherry picked from https://android-review.googlesource.com/q/commit:4ea2ccf33a39348d7fb7867ad7edf498dc483343)
    Merged-In: I8171dbb0ad8316b0c5874de5c8d0a8962f16096d
    Change-Id: I8171dbb0ad8316b0c5874de5c8d0a8962f16096d

--- remote_provisioning/hwtrust/src/cbor/dice/field_value.rs
+++ remote_provisioning/hwtrust/src/cbor/dice/field_value.rs
@@ -1,7 +1,21 @@
 //! This module defines a helper for parsing fields in a CBOR map.
 
-use anyhow::{anyhow, bail, Result};
 use coset::cbor::value::Value;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum FieldValueError {
+    #[error("expected a value but none was found")]
+    Missing,
+    #[error("expected bytes but found `{0:?}`")]
+    NotBytes(Value),
+    #[error("expected string but found `{0:?}`")]
+    NotString(Value),
+    #[error("expected null but found `{0:?}`")]
+    NotNull(Value),
+    #[error("expected i64 but found `{0:?}`")]
+    NotI64(Value),
+}
 
 pub(super) struct FieldValue {
     name: &'static str,
@@ -13,62 +27,56 @@ impl FieldValue {
         Self { name, value: None }
     }
 
-    pub fn set(&mut self, value: Value) -> Result<()> {
-        if let Some(existing) = &self.value {
-            bail!("Duplicate values for {}: {:?} and {:?}", self.name, existing, value);
-        } else {
-            self.value = Some(value);
-            Ok(())
-        }
+    pub fn name(&self) -> &str {
+        self.name
+    }
+
+    pub fn get(&self) -> &Option<Value> {
+        &self.value
+    }
+
+    pub fn set(&mut self, value: Value) {
+        self.value = Some(value)
     }
 
     pub fn is_bytes(&self) -> bool {
         self.value.as_ref().map_or(false, |v| v.is_bytes())
     }
 
-    pub fn into_optional_bytes(self) -> Result<Option<Vec<u8>>> {
+    pub fn into_optional_bytes(self) -> Result<Option<Vec<u8>>, FieldValueError> {
         self.value
-            .map(|v| {
-                if let Value::Bytes(b) = v {
-                    Ok(b)
-                } else {
-                    bail!("{}: expected bytes, got {:?}", self.name, v)
-                }
+            .map(|v| match v {
+                Value::Bytes(b) => Ok(b),
+                _ => Err(FieldValueError::NotBytes(v)),
             })
             .transpose()
     }
 
-    pub fn into_bytes(self) -> Result<Vec<u8>> {
-        require_present(self.name, self.into_optional_bytes())
+    pub fn into_bytes(self) -> Result<Vec<u8>, FieldValueError> {
+        require_present(self.into_optional_bytes())
     }
 
-    pub fn into_optional_string(self) -> Result<Option<String>> {
+    pub fn into_optional_string(self) -> Result<Option<String>, FieldValueError> {
         self.value
-            .map(|v| {
-                if let Value::Text(s) = v {
-                    Ok(s)
-                } else {
-                    bail!("{}: expected text, got {:?}", self.name, v)
-                }
+            .map(|v| match v {
+                Value::Text(s) => Ok(s),
+                _ => Err(FieldValueError::NotString(v)),
             })
             .transpose()
     }
 
-    pub fn into_string(self) -> Result<String> {
-        require_present(self.name, self.into_optional_string())
+    pub fn into_string(self) -> Result<String, FieldValueError> {
+        require_present(self.into_optional_string())
     }
 
-    pub fn is_null(&self) -> Result<bool> {
+    pub fn is_null(&self) -> Result<bool, FieldValueError> {
         // If there's no value, return false; if there is a null value, return true; anything else
         // is an error.
         self.value
             .as_ref()
-            .map(|v| {
-                if *v == Value::Null {
-                    Ok(true)
-                } else {
-                    bail!("{}: expected null, got {:?}", self.name, v)
-                }
+            .map(|v| match *v {
+                Value::Null => Ok(true),
+                _ => Err(FieldValueError::NotNull(v.clone())),
             })
             .unwrap_or(Ok(false))
     }
@@ -77,17 +85,17 @@ impl FieldValue {
         self.value.as_ref().map_or(false, |v| v.is_integer())
     }
 
-    pub fn into_optional_i64(self) -> Result<Option<i64>> {
+    pub fn into_optional_i64(self) -> Result<Option<i64>, FieldValueError> {
         self.value
             .map(|v| {
                 let value =
                     if let Value::Integer(i) = v { i128::from(i).try_into().ok() } else { None };
-                value.ok_or_else(|| anyhow!("{}: expected integer, got {:?}", self.name, v))
+                value.ok_or_else(|| FieldValueError::NotI64(v))
             })
             .transpose()
     }
 }
 
-fn require_present<T>(name: &'static str, value: Result<Option<T>>) -> Result<T> {
-    value.and_then(|opt| opt.ok_or_else(|| anyhow!("{} must be present", name)))
+fn require_present<T>(value: Result<Option<T>, FieldValueError>) -> Result<T, FieldValueError> {
+    value.and_then(|opt| opt.ok_or(FieldValueError::Missing))
 }
